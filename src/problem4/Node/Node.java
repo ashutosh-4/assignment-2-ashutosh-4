f3fc94b0703986933d2276f8c08e51e8918bdd51
ashutosh-4 <ashutosh.chauhan_cs18@gla.ac.in>
2020-03-28 18:20:04 +0530

    updated node class

--- src/problem4/Node/Node.java
+++ src/problem4/Node/Node.java
@@ -1,4 +1,26 @@
 package problem4.Node;
 
 public class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
